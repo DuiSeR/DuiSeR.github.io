96a9409462bf475ef1043ef313ab0210654eedc7
DuiSeR <dushkafront@gmail.com>
2023-05-25 22:10:24 +0300

    added section(form)+media

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ document.querySelector(".burger").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".nav").classList.toggle("open");
 });
+
+const input = document.querySelector("#service-input");
+[
+  ["#development", "Разработка сайта"],
+  ["#bot", "Чат бот"],
+  ["#design", "Дизайн"],
+].forEach(
+  ([id, value]) =>
+    (document.querySelector(id).onclick = () => (input.value = value))
+);
+
+const input2 = document.querySelector("#communication-input");
+[
+  ["#mail", "Почта"],
+  ["#phone", "Телефон"],
+  ["#messenger", "Мессенджер"],
+].forEach(
+  ([id, value]) =>
+    (document.querySelector(id).onclick = () => (input2.value = value))
+);
